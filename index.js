a87d4d23539e8b1964d9a20b64244462a28673f6
bsowmiya=567 <sowzhasai25@gmail.com>
2024-01-07 12:29:10 +0530

    welcome msg added

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 const PORT = 5000;
 app.use(cors())
 app.use(express.json())
+app.get('/',(req,res)=>{
+    res.send("Welcome to Hall Booking App 🎉")
+})
 app.use('/api/rooms',roomrouter)
 app.listen(PORT,()=>{
     console.log("my App is listen port",PORT);
